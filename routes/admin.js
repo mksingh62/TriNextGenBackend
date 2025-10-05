003c4337052dccdf4441fafed8b15676496012ea
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-10-05 12:38:27 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -39,9 +39,6 @@ router.post('/register', async (req, res) => {
   }
 });
 
-app.get('/hello', (req, res) => {
-  res.send('API is running');
-});
 // @route  POST api/admin/login
 // @desc   Authenticate admin & get token
 // @access Public
