73a3e054557ded6bb8e8d684870c82b16d26c897
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-12-17 13:28:49 +0530

    Change environment variable from MONGO_URI to MONGODB_URI

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 
 if (!MONGO_URI) {
   throw new Error("❌ MONGO_URI not defined");
