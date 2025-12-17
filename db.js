e4a61eaac640b3b5c5edf3d2f977c19e0dce6d05
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-12-17 12:53:20 +0530

    Implement MongoDB connection with Mongoose

---
+++ db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("❌ MONGO_URI not defined");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+module.exports = connectDB;
