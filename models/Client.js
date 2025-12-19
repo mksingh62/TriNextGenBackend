0ec73824fb759f8deab6c7040ef64d0b558ab988
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-12-19 15:42:21 +0530

    Update Client.js

--- models/Client.js
+++ models/Client.js
@@ -3,18 +3,12 @@ const mongoose = require("mongoose");
 const ClientSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: String,
     phone: String,
     address: String,
-
-    status: {
-      type: String,
-      enum: ["active", "inactive"],
-      default: "active",
-    },
-
+    status: { type: String, default: "Active" },
     advance: { type: Number, default: 0 },
-    totalEarnings: { type: Number, default: 0 },
+    totalEarnings: { type: Number, default: 0 }
   },
   { timestamps: true }
 );
