70c39c6875276db81f1dcaf3930c1b384d2b7032
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-12-19 13:35:28 +0530

    Create Client.js

---
+++ models/Client.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ClientSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String },
+    address: { type: String },
+
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+
+    advance: { type: Number, default: 0 },
+    totalEarnings: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Client", ClientSchema);
