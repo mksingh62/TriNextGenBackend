703b38e8f5a7720433b710c2837d94c613e924af
Mohit Kumar Singh <135017522+mksingh62@users.noreply.github.com>
2025-12-19 13:38:09 +0530

    Update Client.js

--- models/Client.js
+++ models/Client.js
@@ -4,8 +4,8 @@ const ClientSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
-    phone: { type: String },
-    address: { type: String },
+    phone: String,
+    address: String,
 
     status: {
       type: String,
